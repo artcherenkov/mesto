233737b10b45bab25fc1d033140587e7a5c17f54
Artem Cherenkov <art.cherenkov@gmail.com>
2021-03-31 11:03:51 +0700

    feat: extract data to file

---
+++ scripts/data.js
@@ -0,0 +1,38 @@
+export const INITIAL_CARDS = [
+  {
+    id: 1,
+    title: "Архыз",
+    imageUrl:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+  },
+  {
+    id: 2,
+    title: "Челябинская область",
+    imageUrl:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+  },
+  {
+    id: 3,
+    title: "Иваново",
+    imageUrl:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+  },
+  {
+    id: 4,
+    title: "Камчатка",
+    imageUrl:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
+  },
+  {
+    id: 5,
+    title: "Холмогорский район",
+    imageUrl:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
+  },
+  {
+    id: 6,
+    title: "Байкал",
+    imageUrl:
+      "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+  },
+];
