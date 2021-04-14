8028d64dbb023494b3d0e9ee4501e87b34b2f677
Artem Cherenkov <art.cherenkov@gmail.com>
2021-04-14 20:46:37 +0700

    feat: add Popup esc key press and outside click handlers

--- components/Popup.js
+++ components/Popup.js
@@ -8,6 +8,10 @@ export default class Popup {
     this.open = this.open.bind(this);
     this.close = this.close.bind(this);
     this.setEventListeners = this.setEventListeners.bind(this);
+    this.setCloseHandlers = this.setCloseHandlers.bind(this);
+    this.removeCloseHandlers = this.removeCloseHandlers.bind(this);
+    this._handleEscKeyPress = this._handleEscKeyPress.bind(this);
+    this._handleOutsideClick = this._handleOutsideClick.bind(this);
   }
 
   setEventListeners() {
@@ -16,11 +20,33 @@ export default class Popup {
 
   open() {
     this._popupElement.classList.add("popup_opened");
+    this.setCloseHandlers();
   }
 
   close() {
     this._popupElement.classList.remove("popup_opened");
+    this.removeCloseHandlers();
   }
 
-  _handleEscKeyPress() {}
+  _handleEscKeyPress(evt) {
+    if (evt.key === "Escape") {
+      this.close();
+    }
+  }
+
+  _handleOutsideClick(evt) {
+    if (evt.target.classList.contains("popup_opened")) {
+      this.close();
+    }
+  }
+
+  setCloseHandlers() {
+    document.addEventListener("keydown", this._handleEscKeyPress);
+    this._popupElement.addEventListener("click", this._handleOutsideClick);
+  }
+
+  removeCloseHandlers() {
+    document.removeEventListener("keydown", this._handleEscKeyPress);
+    this._popupElement.removeEventListener("click", this._handleOutsideClick);
+  }
 }
