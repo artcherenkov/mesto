567a70463e498e4ea2163925f9c156c683bfeadd
Artem Cherenkov <art.cherenkov@gmail.com>
2021-04-14 22:54:41 +0700

    refactor: delete unused scripts

--- scripts/image-popup.js
+++
@@ -1,6 +0,0 @@
-import { closePopup } from "../pages";
-
-const popupElement = document.querySelector(".popup_type_fullscreen-image");
-const closePopupButton = popupElement.querySelector(".popup__close-button");
-
-closePopupButton.addEventListener("click", () => closePopup(popupElement));
