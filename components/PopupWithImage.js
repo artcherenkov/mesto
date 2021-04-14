e641ea448d433c28f72b86d26d4631be1a63e96c
Artem Cherenkov <art.cherenkov@gmail.com>
2021-04-14 20:40:45 +0700

    feat: add logic to PopupWithImage class

--- components/PopupWithImage.js
+++ components/PopupWithImage.js
@@ -3,9 +3,18 @@ import Popup from "./Popup.js";
 export default class PopupWithImage extends Popup {
   constructor(popupSelector) {
     super(popupSelector);
+
+    this._imageElement = this._popupElement.querySelector(".popup__image");
+    this._captionElement = this._popupElement.querySelector(
+      ".popup__image-caption"
+    );
   }
 
-  open() {
+  open({ title, imageUrl }) {
+    this._imageElement.src = imageUrl;
+    this._imageElement.alt = title;
+    this._captionElement.textContent = title;
+
     super.open();
   }
 }
