508a5566eb7c481fe5109998f738fef33e80295b
Artem Cherenkov <art.cherenkov@gmail.com>
2021-04-14 20:30:06 +0700

    feat: add logic to Popup class

--- components/Popup.js
+++ components/Popup.js
@@ -1,13 +1,26 @@
 export default class Popup {
   constructor(popupSelector) {
     this._popupElement = document.querySelector(popupSelector);
+    this._closePopupBtn = this._popupElement.querySelector(
+      ".popup__close-button"
+    );
+
+    this.open = this.open.bind(this);
+    this.close = this.close.bind(this);
+    this.setEventListeners = this.setEventListeners.bind(this);
   }
 
-  setEventListeners() {}
+  setEventListeners() {
+    this._closePopupBtn.addEventListener("click", this.close);
+  }
 
-  open() {}
+  open() {
+    this._popupElement.classList.add("popup_opened");
+  }
 
-  close() {}
+  close() {
+    this._popupElement.classList.remove("popup_opened");
+  }
 
   _handleEscKeyPress() {}
 }
